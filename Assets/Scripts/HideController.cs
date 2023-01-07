6f61784c80a2a2e2dcf97698e239686eb866d3bf
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2023-01-07 14:40:42 +0100

    Update HideController.cs

--- Assets/Scripts/HideController.cs
+++ Assets/Scripts/HideController.cs
@@ -9,11 +9,15 @@ public class HideController : MonoBehaviour
     [SerializeField] private GameObject canvasToHide;
     void OnTriggerEnter2D(Collider2D col)
     {
-        canvasToHide.gameObject.SetActive(true);
+        if (col.compareTag("player")) {
+            canvasToHide.gameObject.SetActive(true);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        canvasToHide.gameObject.SetActive(false);
+        if (col.compareTag("player")) {
+            canvasToHide.gameObject.SetActive(false);
+        }
     }
 }
