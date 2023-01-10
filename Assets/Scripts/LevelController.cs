0d53c195d6a13702440edebb737e5ce4675eb6fe
JaworroChanUwU <blazejjaworskixd@gmail.com>
2023-01-10 23:00:23 +0100

    LevelController

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Events;
@@ -8,11 +10,12 @@ public class LevelController : MonoBehaviour
 {
     public static LevelController Instance { get; private set; }
 
-    [SerializeField] private Animator transition;
-    [SerializeField] float transitionTime = 1.0f;
+    [SerializeField] private Animator animator;
 
-    [SerializeField] private UnityEvent onSceneOpenTransition;
-    [SerializeField] private UnityEvent<int> onSceneCloseTransition;
+    private string nextSceneName;
+
+    private const string FADE_IN = "level_crossfade_close";
+    private const string FADE_OUT = "level_crossfade_open";
 
     private void Awake()
     {
@@ -21,13 +24,24 @@ public class LevelController : MonoBehaviour
         DontDestroyOnLoad(Instance.gameObject);
     }
 
-    public void ClosingAnimation(string sceneName)
+    private void Start()
     {
-        int levelIndex = SceneUtility.GetBuildIndexByScenePath(sceneName);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        animator.Play(FADE_OUT);
+    }
 
-        SceneManager.LoadScene(levelIndex);
-
-        onSceneCloseTransition?.Invoke(levelIndex);
-        onSceneOpenTransition?.Invoke();
+    public void ChangeScene(string scene)
+    {
+        nextSceneName = scene;
+        animator.Play(FADE_IN);
+    }
+    
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
     }
 }
